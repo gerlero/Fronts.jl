2009c29c3ce7d04381c8c415c7b1052a86424635
Gabriel Gerlero <gerlero@users.noreply.github.com>
2021-07-31 20:43:00 -0300

    Use porous models in examples

--- examples/HF135_richards.jl
+++ examples/HF135_richards.jl
@@ -2,6 +2,7 @@
 module ExampleHF135Richards
 
 using Fronts
+using Fronts.PorousModels
 using Plots
 
 # Wetting of an HF135 membrane, Van Genuchten model
@@ -17,32 +18,9 @@ n = 2.3521
 hi = -30.591486389337
 hb = 0
 
-# Define the Van Genuchten model
-m = 1-1/n
+model = VanGenuchten(n=n, α=α, k=k, θr=θr, θs=θs)
 
-toθ(h) = θr + toSe(h)*(θs - θr)
-
-function toSe(h)
-    if h≥0 return one(h) end
-    1/(1+abs(α*h)^n)^m
-end
-
-Ks = Fronts.D._asKs(k=k)
-l = 0.5
-
-function K(h)
-    if h≥0 return one(h) end
-    Se = toSe(h)
-    Ks*Se^l*(1-(1-Se^(1/m))^m)^2
-end
-
-function C(h)
-    if h≥0 return zero(h) end
-    Se = toSe(h)
-    α*m/(1-m)*(θs - θr)*Se^(1/m)*(1-Se^(1/m))^m
-end
-
-prob = DirichletProblem(RichardsEquation(C=C, K=K), i=hi, b=hb)
+prob = DirichletProblem(RichardsEquation(model), i=hi, b=hb)
 
 h = solve(prob)
 
@@ -51,7 +29,7 @@ r = range(0, 0.05, length=500)
 plt = plot(r, r -> h(r,60), label="t = 60 s", xguide="r [m]", yguide="h [m]")
 display(plt)
 
-plt = plot(r, r -> toθ(h(r,60)), label="t = 60 s", xguide="r [m]", yguide="θ")
+plt = plot(r, r -> θh(model, h(r,60)), label="t = 60 s", xguide="r [m]", yguide="θ")
 display(plt)
 
 plt = plot(r[2:end], r -> flux(h, r, 60), label="t = 60 s", xguide="r [m]", yguide="U [m/s]")
