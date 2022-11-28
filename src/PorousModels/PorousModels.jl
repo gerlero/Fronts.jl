dc92171366be16f074c5e38bb21eba3d01007845
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-11-28 15:18:08 -0300

    Add LETxs PorousModel

--- src/PorousModels/PorousModels.jl
+++ src/PorousModels/PorousModels.jl
@@ -8,9 +8,11 @@ include("base.jl")
 include("common.jl")
 include("brooksandcorey.jl")
 include("vangenuchten.jl")
+include("letxs.jl")
 
 export UnsaturatedFlowModel, hθ, θh, Ch, Kh, Cθ, Kθ, Dθ
 export BrooksAndCorey
 export VanGenuchten
+export LETxs
 
 end
