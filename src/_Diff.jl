cd65c33a9dd4ffec8b2efc5aa3f516844f46a86f
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-11-28 14:04:34 -0300

    Update internal differentiation module

--- src/_Diff.jl
+++ src/_Diff.jl
@@ -4,16 +4,15 @@ import ForwardDiff
 using DiffResults: DiffResult
 import DiffResults
 
+using ForwardDiff: derivative
+
+@inline function value_and_derivative(f, x::Real)
+    return f(x), derivative(f, x)
+end
 
 @inline function value_and_derivative(f, Y::Type, x::Real)
     diffresult = ForwardDiff.derivative!(DiffResult(zero(Y), zero(Y)), f, x)
     return DiffResults.value(diffresult), DiffResults.derivative(diffresult)
-end
-
-const derivative = ForwardDiff.derivative
-
-@inline function value_and_derivative(f, x::Real)
-    return f(x), derivative(f, x)
 end
 
 export derivative, value_and_derivative
