c3627cfb1312e345301d875eefc92087176c201d
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-03-03 13:06:42 -0300

    Test differentiation module

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Fronts
+using Fronts._Diff
 using Fronts.PorousModels
 using Fronts.ParamEstim
 using Test
-using Fronts._Diff: derivative
+
+import ForwardDiff
 using OrdinaryDiffEq: ODEFunction, ODEProblem
 using OrdinaryDiffEq.DiffEqBase: NullParameters
 using StaticArrays: @SVector, SVector
@@ -10,6 +12,8 @@ using StaticArrays: @SVector, SVector
 using Plots: plot
 
 @testset "Fronts.jl" begin
+    include("test_Diff.jl")
+    include("test_PorousModels.jl")
     include("test_dirichlet.jl")
     include("test_flowrate.jl")
     include("test_cauchy.jl")
@@ -19,6 +23,5 @@ using Plots: plot
     include("test_pseudospectral.jl")
     include("test_inverse.jl")
     include("test_ParamEstim.jl")
-    include("test_PorousModels.jl")
     include("test_plot.jl")
 end
---
+++ test/test_Diff.jl
@@ -0,0 +1,20 @@
+@testset "_Diff" begin
+    # HF135 membrane, Van Genuchten model
+    # Data from Buser (PhD thesis, 2016)
+    # http://hdl.handle.net/1773/38064
+    θr = 0.0473
+    θs = 0.945
+    k = 5.50e-13  # m^2
+    α = 0.2555  # 1/m
+    n = 2.3521
+
+    model = VanGenuchten(n=n, α=α, k=k, θr=θr, θs=θs)
+
+    D(θ) = Dθ(model, θ)
+
+    for θ in range(θr + eps(θr), θs - eps(θs), length=10)
+        D_, dD_dθ = value_and_derivative(D, typeof(θ), θ)
+        @test D_ == D(θ)
+        @test dD_dθ == derivative(D, θ) == ForwardDiff.derivative(D, θ)
+    end
+end
