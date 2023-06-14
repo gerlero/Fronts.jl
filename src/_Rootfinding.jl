130900b133eb5a2cbc69433fd1241d17c036f16c
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-06-14 00:06:14 -0300

    Refactor rootfinding module with ResumableFunctions' yieldfrom

--- src/_Rootfinding.jl
+++ src/_Rootfinding.jl
@@ -3,10 +3,9 @@ module _Rootfinding
 using ResumableFunctions
 using ArgCheck: @argcheck
 
-@resumable function bracket_bisect(xa, xb, ya=missing, yb=missing; growth_factor=2)
+
+@resumable function _find_bracket(xa, xb, ya=missing, yb=missing; growth_factor=2)
     @argcheck xa != xb
-    @argcheck ismissing(ya) || !iszero(ya)
-    @argcheck ismissing(yb) || !iszero(yb)
     @argcheck growth_factor >= 1
 
     if ismissing(ya)
@@ -16,12 +15,33 @@ using ArgCheck: @argcheck
         yb = @yield xb
     end
 
+    @argcheck !iszero(ya) DomainError
+    @argcheck !iszero(yb) DomainError
+
     while sign(ya) == sign(yb)
         x = xb + growth_factor*(xb - xa)
         y = @yield x
         xa, xb = xb, x
         ya, yb = yb, y
     end
+
+    return xa, xb, ya, yb
+end
+
+@resumable function _bisect(xa, xb, ya=missing, yb=missing)
+    @argcheck xa != xb
+
+    if ismissing(ya)
+        ya = @yield xa
+    end
+    if ismissing(yb)
+        yb = @yield xb
+    end
+
+    @argcheck !iszero(ya) DomainError
+    @argcheck !iszero(yb) DomainError
+
+    @argcheck sign(ya) != sign(yb) DomainError
 
     while true
         x = (xa + xb)/2
@@ -34,6 +54,11 @@ using ArgCheck: @argcheck
             yb = y
         end
     end
+end
+
+@resumable function bracket_bisect(xa, xb, ya=missing, yb=missing; growth_factor=2)
+    bracket = @yieldfrom _find_bracket(xa, xb, ya, yb; growth_factor=growth_factor)
+    @yieldfrom _bisect(bracket...)
 end
 
 export bracket_bisect
